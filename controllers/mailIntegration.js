4f2784db02714d9988cbb68cf1e445dfc9d4cb6c
Mayank <mayank.sharma.dev06@gmail.com>
2026-01-24 14:50:40 +0530

    Update nodmailer Transport

--- controllers/mailIntegration.js
+++ controllers/mailIntegration.js
@@ -5,7 +5,9 @@ require("dotenv").config();
 
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: 'smtp.gmail.com',
+  port: '587',
+  secure: false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD, // Gmail App Password
